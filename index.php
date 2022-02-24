57c35e11e98548e2e5c8b77f52f13202a1279157
AngeloLista <abstractplayer@gmail.com>
2022-02-24 16:35:20 +0100

    created php paragraph and printed through html

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php
+$paragrafo = '<p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>'
+?>
+
+<body>
+    <main>
+        <p><?php echo $paragrafo ?></p>
+    </main>
+</body>
+
+</html>
+
+
+
+
+
+<!--
+Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+-->
